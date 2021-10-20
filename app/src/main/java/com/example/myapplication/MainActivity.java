7b91479a14cf42a75b0d7e0341aa781321aae9c8
Михаил Скляров <mikhail.29.06.2001@gmail.com>
2021-10-20 22:05:30 +0300

    добавил файлы

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    String[] roles = { "Студент", "Преподаватель" };
+
+    Spinner spinnerRoles;
+    Button btnLogIn;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinnerRoles = findViewById(R.id.spinner);
+        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, roles);
+        // Определяем разметку для использования при выборе элемента
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Применяем адаптер к элементу spinner
+        spinnerRoles.setAdapter(adapter);
+
+
+        btnLogIn = (Button) findViewById(R.id.btnLogIn);
+        btnLogIn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogIn:
+                if(spinnerRoles.getSelectedItem().toString() == "Студент") {
+                    Intent intent1 = new Intent(this, StudentPage.class);
+                    startActivity(intent1);
+                }
+                else
+                {
+                    Intent intent2 = new Intent(this, teacherPage.class);
+                    startActivity(intent2);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
